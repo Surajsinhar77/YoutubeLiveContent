bd42e923c33289ee0fbef8fea08a4a14501e3b50
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-04 21:09:47 +0530

    d

--- auth-application/src/Components/Profile.jsx
+++ auth-application/src/Components/Profile.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import { authApp } from '../common/AuthContext'
+import { AiOutlineCheck } from "react-icons/ai";
 
 function Profile() {
     const {userData} = authApp();
     
+    function checkChangeIninput(e){
+        // e.preventDefault();
+        if (e.target.value == ""){
+            return true;
+        }  
+        return false;
+    }
+
+    const fullname = document.querySelector('.fullName');
+    const email = document.querySelector('.email');
+    const phoneno = document.querySelector('.phoneno');
+
+    fullname.addEventListener('change', ()=>{
+        
+    })
+
     return (
         <div>
             <div className="container w-4/5 m-auto">
@@ -13,21 +30,32 @@ function Profile() {
                     <div className="row w-2/4 flex justify-between ">
                         <label htmlFor="">ID <span>{userData._id}</span></label>
                     </div>
-                    <div className="row w-2/4 flex justify-between ">
+                    <div className="row w-3/4 flex justify-between ">
                         <label htmlFor="">FullName</label>
-                        <input type="text" placeholder={userData.fullName} className='border border-gray-300 px-2 py-1'/>
+                        <div className='flex flex-row items-center'>
+                            <input type="text" placeholder={userData.fullName} className='fullName border border-gray-300 px-2 py-1'/>
+                            <span className='pl-4 text-green-400 text-2xl'><AiOutlineCheck/></span>
+                        </div>
                     </div>
-                    <div className="row w-2/4 flex justify-between ">
+                    <div className="row w-3/4 flex justify-between ">
                         <label htmlFor="">Email</label>
-                        <input type="text" placeholder={userData.email} className='border border-gray-300 px-2 py-1'/>
+                        <div className='flex flex-row items-center'>
+                            <input type="text" placeholder={userData.email} className='email border border-gray-300 px-2 py-1'/>
+                            <span className='pl-4 text-green-400 text-2xl'><AiOutlineCheck/></span>
+                        </div>
                     </div>
-                    <div className="row w-2/4 flex justify-between ">
+                    <div className="row w-3/4 flex justify-between ">
                         <label htmlFor="">Phone</label>
-                        <input type="text" placeholder={userData.phoneno} className='border border-gray-300 px-2 py-1'/>
+                        <div className='flex flex-row items-center'>
+                            <input type="text" placeholder={userData.phoneno} className='phoneno border border-gray-300 px-2 py-1'/>
+                            <span className='pl-4 text-green-400 text-2xl'><AiOutlineCheck/></span>
+                        </div>
                     </div>
 
                     <div className="actionButtons">
-                        div.
+                        <div className="saveData">
+                            <button className='border text-gray-500 border-blue-400 px-2 py-1 rounded'>Save</button>
+                        </div>
                     </div>
                 </div>
             </div>
